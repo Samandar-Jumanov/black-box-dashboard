d7f13dee4fdbbcfd6dec3cd2acc9057cdfebda1b
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-03-22 12:58:27 +0500

    updated keys logic

--- src/utils/generateKey.tsx
+++ src/utils/generateKey.tsx
@@ -1,27 +1,48 @@
 import { v4 as uuid } from 'uuid';
 import prisma from '../../prisma/prisma';
 
+const generateKey = (): string => {
+  const key = uuid();
+  return key.replace(/-/g, '').slice(0, 16);
+};
 
- const createKey  =  async ( userId : string  , name ? : string  , description ? : string) =>{
-      const key = uuid();
-      const  secretKey = key.replace(/-/g, '').slice(0, 16);
+const createKey = async (userId: string, name?: string, description?: string) => {
+  let unique = false;
+  let key = "";
+  let attempts = 0; 
 
+  while (!unique && attempts < 50) { 
+    key = generateKey();
 
-      const newApiKey =  await prisma.apiKey.create({
-          data : {
-              name : name ? name :  "Default secret key ",
-              description :  description ? description : "This is a default secret key . Please do not share it!",
-              key : secretKey,
-              user : {
-                 connect : {
-                      id : userId
-                 }
-              }
-          }
-      })
+    const existingKey = await prisma.apiKey.findUnique({
+      where: { key: key }
+    });
 
-      return  newApiKey.id ? "Success" : "Failed"
-      
-}
+    if (!existingKey) {
+      unique = true;
+    } else {
+      attempts++;
+    }
+  }
 
-export default createKey
+  if (!unique) {
+    return "Failed to generate a unique key after multiple attempts.";
+  }
+
+  const newApiKey = await prisma.apiKey.create({
+    data: {
+      name: name ? name : "Default secret key",
+      description: description ? description : "This is a default secret key. Please do not share it!",
+      key: key,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  });
+
+  return newApiKey.id ? "Success" + ` ${attempts}` : "Failed" + + ` ${attempts}`;
+};
+
+export default createKey;
