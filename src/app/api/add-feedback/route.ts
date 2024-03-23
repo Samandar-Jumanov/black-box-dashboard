39f2ef973f88bad979f0e1cc2de9f7b6c5a9c919
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-03-23 12:08:12 +0500

    updated ad feedback

--- src/app/api/add-feedback/route.ts
+++ src/app/api/add-feedback/route.ts
@@ -15,7 +15,7 @@ export const POST = async ( request : Request , response : Response) =>{
     const data  : FeedBackBody  = await request.json();
     const { userEmail , userName ,  description , loggedEmail , password  , key } : FeedBackBody = data;
 
-    if(!userEmail || !userName || !description || !loggedEmail || !password) {
+    if(!userEmail || !userName || !description   || !loggedEmail || !password || !key) {
           throw new Error("Invalid inputs")
     };
 
@@ -40,27 +40,30 @@ export const POST = async ( request : Request , response : Response) =>{
 
 
         for(let i = 0 ; i < user.apiKeys.length  ; i++){
-             
-            if(user.apiKeys[i].key !== key) {
-                return new Response("Invalid key" , { status : 403})
-            }
-        };
-
-
-        const newFeedBack = await prisma.feedBacks.create({
-              data : {
-                ... data
+            if(user.apiKeys[i].key === key) {
+           
+                const newFeedBack = await prisma.feedBacks.create({
+                    data : {
+                      userEmail : userEmail,
+                      userName :userName,
+                      description : description
+                    }
+              })
+      
+              const responseData = {
+                 userEmail : newFeedBack.userEmail,
+                 userName : newFeedBack.userName,
+                 description : newFeedBack.description
               }
-        })
+      
+              
+              return new Response(JSON.stringify(responseData) , { status : 201})
 
-        const responseData = {
-           userEmail : newFeedBack.userEmail,
-           userName : newFeedBack.userName,
-           description : newFeedBack.description
+            }
         }
 
-        
-        return new Response(JSON.stringify(responseData) , { status : 201})
+        return new Response("Invalid key")
+       
     }catch( err : any ) {
         console.log({
              error : err 
