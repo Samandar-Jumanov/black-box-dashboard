7c3d0553f60122b9caf6ef994e81e54f7fc491f0
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-03-27 21:12:36 +0500

    updateing feedbacks

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -20,7 +20,7 @@ const Collections = () => {
   useEffect(() => {
     async function fetchAllCollections() {
       if (session?.user?.email) {
-        const url = `http://localhost:3000/api/all-collections/${session.user.email}`;
+        const url = `https://black-box-dashboard.vercel.app/api/all-collections/${session.user.email}`;
         try {
           const response = await fetch(url);
           if (!response.ok) {
